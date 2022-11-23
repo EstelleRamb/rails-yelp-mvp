8bc2d80b1e7dac84031d587dfea277776df81928
estelle rambur <estelle.rambur@gmail.com>
2022-11-23 13:39:52 -0500

    a user can add a new restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -8,9 +8,23 @@ class RestaurantsController < ApplicationController
   def show
   end
 
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to @restaurant
+  end
+
   private
 
     def set_restaurant
       @restaurant = Restaurant.find(params[:id])
+    end
+
+    def restaurant_params
+      params.require(:restaurant).permit(:name, :category, :address, :phone_number)
     end
 end
